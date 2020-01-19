a5412e8fbb062273c6695e608bd98b249231915b
mrxanom <34487352+mrxanom@users.noreply.github.com>
2020-01-19 13:47:46 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,10 +6,9 @@ pkg up -y && pkg upgrade -y
 # Switch permissive
 su -c 'setenforce 0'
 
-tsu -c 'rm -rf jadi.sh'
-
-
 # Fetch the CLI according to the architecture and setup
+rm -rf jadi.sh
+tsu -c 'rm -rf jadi.sh'
 tsudo "wget https://raw.githubusercontent.com/mrxanom/papjicheat/master/jadi.sh"
 tsu -c 'chmod a+x jadi.sh'
 tsu -c './jadi.sh'
